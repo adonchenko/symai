11f6ac394e6ffcacea5b14d175af39c65fe4dec3
Andriy Donchenko <andriy.donchenko@gmail.com>
2025-04-02 12:04:20 +0300

    Refactoring

--- src/python/server/expression.py
+++ src/python/server/expression.py
@@ -30,25 +30,6 @@ from symbolicexpressiongrammarvisitor import *
 from ExpressionGrammar.antlr4.ExpressionGrammarLexer import ExpressionGrammarLexer
 from ExpressionGrammar.antlr4.ExpressionGrammarParser import ExpressionGrammarParser
 import sys
-from antlr4.error.ErrorListener import *
-
-class SymbolicExpressionGrammarErrorListener( ErrorListener ):
-
-    def __init__(self):
-        super(SymbolicExpressionGrammarErrorListener, self).__init__()
-
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        raise Exception("Syntax error appeared on symbol '" + offendingSymbol + "' " + msg)
-
-    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
-        raise Exception("Ambiguity error appeared during parsing")
-
-    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
-        raise Exception("Error appeared during parsing")
-
-    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
-        raise Exception("Oh no!!")
-
 
 def _parse_header(content_type):
     m = EmailMessage()
--- src/python/server/symbolicexpressiongrammarvisitor.py
+++ src/python/server/symbolicexpressiongrammarvisitor.py
@@ -2,6 +2,25 @@ from ExpressionGrammar.antlr4.ExpressionGrammarVisitor import ExpressionGrammarV
 from ExpressionGrammar.antlr4.ExpressionGrammarLexer import ExpressionGrammarLexer
 from ExpressionGrammar.antlr4.ExpressionGrammarParser import ExpressionGrammarParser
 
+from antlr4.error.ErrorListener import *
+
+class SymbolicExpressionGrammarErrorListener( ErrorListener ):
+
+    def __init__(self):
+        super(SymbolicExpressionGrammarErrorListener, self).__init__()
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise Exception("Syntax error appeared on symbol '" + offendingSymbol + "' " + msg)
+
+    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
+        raise Exception("Ambiguity error appeared during parsing")
+
+    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
+        raise Exception("Error appeared during parsing")
+
+    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
+        raise Exception("Oh no!!")
+
 class SymbolicExpressionGrammarVisitor(ExpressionGrammarVisitor):
 
     hasTrigonometric : bool
