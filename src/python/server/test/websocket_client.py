41299849510e31f217ec14445bc7f3676f0bed22
Andriy Donchenko <andriy.donchenko@gmail.com>
2025-07-26 20:54:07 +0300

    websocket client. Low level threading used instead of async io

--- src/python/server/test/websocket_client.py
+++ src/python/server/test/websocket_client.py
@@ -2,43 +2,37 @@
 
 """A websockets client with SymAO UI protocol for python 3.
 """
-import asyncio
 import websockets
-import signal
+from websockets.sync.client import connect
+import _thread
+
 import sys
+import signal
 import argparse
 
 def signal_handler(signal, frame):
     print(f"Signal INT caught")
     sys.exit(0)
 
-# Function to handle the SymAI client
-async def main():
-    parser = argparse.ArgumentParser(description="HTTP Server")
-    parser.add_argument("-p", "--port", dest="port", default=12345, required=False, type=int,
-                        help="Listening port for an SymAI Core Websockets Server")
-    parser.add_argument("-i", "--ip", dest="ip", default="localhost", required=False, help="SymAI Core Websockets Server IP")
-    args = parser.parse_args()
+def receive_messages(websocket):
+    while True:        
+        try:
+            message = websocket.recv()
+            print(">", message)
+        except websockets.exceptions.ConnectionClosed:
+            break
 
-    signal.signal(signal.SIGINT, signal_handler)
+parser = argparse.ArgumentParser(description="HTTP Server")
+parser.add_argument("-p", "--port", dest="port", default=12345, required=False, type=int,
+                    help="Listening port for an SymAI Core Websockets Server")
+parser.add_argument("-i", "--ip", dest="ip", default="localhost", required=False, help="SymAI Core Websockets Server IP")
+args = parser.parse_args()
 
-    try:
-        async with websockets.connect('ws://' + args.ip + ':' + str(args.port)) as websocket:
-            while True:
-                # Prompt the user for a message
-                message = input("Enter command: ")
-                # Send the message to the server
-                await websocket.send(message)
-                # Receive a message from the server
-                response = await websocket.recv()
-                print(f"Received: {response}")
-    except websockets.exceptions.ConnectionClosed:
-        print(f"Quitting. Socket is closed")
-        sys.exit(0)            
-    except Exception as e:
-        print(f"Something wrong. {str(e)} Quitting.")
-        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
-# Run the client
-if __name__ == "__main__":
-    asyncio.run(main())
+with connect('ws://' + args.ip + ':' + str(args.port)) as ws:
+    _thread.start_new_thread(receive_messages,(ws,))
+    while True:
+        toSend = input()
+        ws.send(toSend)
+
