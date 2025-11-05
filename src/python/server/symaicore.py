36b8cdcd0ff40fffb95109cfdee839ef4b01726c
Andriy Donchenko <andriy.donchenko@gmail.com>
2025-11-05 07:30:44 +0200

    improuving processing

--- src/python/server/symaicore.py
+++ src/python/server/symaicore.py
@@ -232,7 +232,13 @@ async def handle_client(websocket):
                                 await websocket.send(res)
                                 if bool(sc.get_debug()):
                                     message = await websocket.recv()
-                                    is_cnt = sc.do_rsp_traversalbeh(str(connected_clients.get(websocket).get_uuid()), message)
+                                    st = message.replace("\t", " ").replace("\r", " ").replace("\n",
+                                                                                               " ").strip().split()
+                                    if len(st) > 0:
+                                        st = message.replace("\t", " ").replace("\r", " ").replace("\n",
+                                                                                                   " ").strip().split()[
+                                            0]
+                                    is_cnt = sc.do_rsp_traversalbeh(str(connected_clients.get(websocket).get_uuid()), st)
                                     if is_cnt:
                                         break
 
