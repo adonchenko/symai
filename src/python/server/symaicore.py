c44439460d21eeb8988d3039d79b33268cbec631
Andriy Donchenko <andriy.donchenko@gmail.com>
2025-11-16 10:57:18 +0200

    quick pathc. should be removed

--- src/python/server/symaicore.py
+++ src/python/server/symaicore.py
@@ -236,6 +236,7 @@ async def handle_client(websocket):
                                     s):
                                 await websocket.send(res)
                                 res = ""
+                                """
                                 if bool(sc.get_debug()):
                                     message = await websocket.recv()
                                     st = message.replace("\t", " ").replace("\r", " ").replace("\n",
@@ -247,7 +248,7 @@ async def handle_client(websocket):
                                     is_cnt = sc.do_rsp_traversalbeh(str(connected_clients.get(websocket).get_uuid()), st)
                                     if is_cnt:
                                         break
-
+                               """
                         except Exception as e:
                             sc.get_logger().error("traversalbeh command failed " + str(e))
                             res = "nok " + str(e)
