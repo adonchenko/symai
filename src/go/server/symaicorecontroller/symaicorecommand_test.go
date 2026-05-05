6321693d87f9a90ec2c8af416a58548ad7849e0e
Andriy Donchenko <andriy.donchenko@gmail.com>
2026-05-05 18:54:09 +0300

    symaicore. UT for environment updated

--- src/go/server/symaicorecontroller/symaicorecommand_test.go
+++ src/go/server/symaicorecontroller/symaicorecommand_test.go
@@ -1,9 +1,10 @@
 package symaicorecontroller
 
 import (
+	"path/filepath"
+
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
-	"path/filepath"
 
 	"testing"
 
@@ -14,8 +15,8 @@ type (
 	DoEnvironmentTestData struct {
 		source     string
 		result     string
-		ctx_key    string		
-	    ctx_result EnvironmentProcessingContext
+		ctx_key    string
+		ctx_result EnvironmentProcessingContext
 		is_error   bool
 	}
 )
@@ -53,49 +54,66 @@ func InitTestConfig(t *testing.T) *Client {
 	return clnt
 }
 
+func ReinitClientTest(clnt *Client) {
+	clnt.ctx = make(map[string]interface{})
+	clnt.send = make(chan []byte)	
+}
+
 func InitDoEnvironmentData(clnt *Client) []DoEnvironmentTestData {
 	testData := make([]DoEnvironmentTestData, 0)
 	testData = append(testData, DoEnvironmentTestData{
-		source:   "{\"content\":\"v==d\"}",
-		result:   "v==d",
+		source:  "{\"content\":\"v==d\"}",
+		result:  "v==d",
 		ctx_key: "environment",
 		ctx_result: EnvironmentProcessingContext{
-		FileBaseData: FileBaseData{
-			Filename: "environment.env",
-			Content:  "v==d",
-			Filepath: filepath.Join(clnt.getBaseTempDir(), "environment.env"),
-		}},
-		is_error: false,	
+			FileBaseData: FileBaseData{
+				Filename: "environment.env",
+				Content:  "v==d",
+				Filepath: filepath.Join(clnt.getBaseTempDir(), "environment.env"),
+			}},
+		is_error: false,
 	})
 	testData = append(testData, DoEnvironmentTestData{
-		source:   "{\"content\":\"v==d\",\"filename\":\"ttt.ggg\"}",
-		result:   "v==d",
+		source:  "{\"content\":\"v==d\",\"filename\":\"ttt.ggg\"}",
+		result:  "v==d",
 		ctx_key: "environment",
 		ctx_result: EnvironmentProcessingContext{
-		FileBaseData: FileBaseData{
-			Filename: "ttt.ggg",
-			Content:  "v==d",
-			Filepath: filepath.Join(clnt.getBaseTempDir(), "environment.env"),
-		}},
-		is_error: false,	
+			FileBaseData: FileBaseData{
+				Filename: "ttt.ggg",
+				Content:  "v==d",
+				Filepath: filepath.Join(clnt.getBaseTempDir(), "environment.env"),
+			}},
+		is_error: false,
 	})
-	return testData	
+	testData = append(testData, DoEnvironmentTestData{
+		source:  "",
+		result:  "{\"filename\":\"environment.env\",\"content\":\"\"}",
+		ctx_key: "environment",
+		ctx_result: EnvironmentProcessingContext{
+			FileBaseData: FileBaseData{
+				Filename: "environment.env",
+				Content:  "",
+				Filepath: filepath.Join(clnt.getBaseTempDir(), "environment.env"),
+			}},
+		is_error: false,
+	})
+	return testData
 }
-
 
 func TestDoEnvironment(t *testing.T) {
 	clnt := InitTestConfig(t)
 	cnf.GetLogger().Info("TestDoEnvironment started")
 	testData := InitDoEnvironmentData(clnt)
-    
+
 	for _, d := range testData {
+		ReinitClientTest(clnt)
 		res, err := doEnvironment(clnt, d.source)
 		if d.is_error {
 			assert.NotNil(t, err, "expected error for environment command")
 		} else {
 			assert.Nil(t, err, "error executing environment command: %v", err)
 			assert.Equal(t, res, d.result, "unexpected response for environment command")
-			assert.Equal(t, clnt.ctx[d.ctx_key].(EnvironmentProcessingContext), d.ctx_result,   "unexpected environment context value")
-		}	
+			assert.Equal(t, clnt.ctx[d.ctx_key].(EnvironmentProcessingContext), d.ctx_result, "unexpected environment context value")
+		}
 	}
 }
